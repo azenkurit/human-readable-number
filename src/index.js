0a5249d003a087785ea929468e58e2320dbc07eb
Eugen <4teachacc@gmail.com>
2022-01-10 16:12:13 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function toReadable (number) {
+    
+  let tensNumber = [ "twenty",  "thirty",  "forty",  "fifty",  "sixty",  "seventy",  "eighty",  "ninety",];  
+  let rez = "";
+
+  if ( number < 10 ) {
+      rez = returnSinglerNumber( number )
+  };
+  if ( number >= 10  && number <20 ) {
+      rez = returnRoundTens( number );
+  }
+  if ( number >= 20 ) {
+    let hundreds = Math.floor( number / 100) ;
+    if ( hundreds > 0 ) {
+        rez += returnSinglerNumber(hundreds)+ " hundred"
+    };
+    number = number - hundreds*100; 
+    if ( number >= 20 ) {
+        let tens = number % 10;
+        rez += " "+tensNumber[ tens ]
+    }    
+  }   
   
+  return rez;
+}
+
+
+function returnSinglerNumber ( num ) {
+    let singleNumbers = [ "zero",   "one",  "two",   "three",  "four",   "five",  "six",  "seven",  "eight",  "nine"];
+    return singleNumbers[ num ];
+}
+function returnRoundTens ( num ) {
+    let singleTens = [ "ten",  "eleven",  "twelve",  "thirteen",  "fourteen",  "fifteen",  "sixteen",  "seventeen",  "eighteen",  "nineteen"];
+    return singleTens[ num - 10]
 }
