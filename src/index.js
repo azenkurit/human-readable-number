3fc0fd912b1b0ccea8853f4c5f685c1248e903e0
Eugen <4teachacc@gmail.com>
2022-01-10 16:24:36 +0300

    feat: 100% passed

--- src/index.js
+++ src/index.js
@@ -1,27 +1,48 @@
 module.exports = function toReadable (number) {
     
-  let tensNumber = [ "twenty",  "thirty",  "forty",  "fifty",  "sixty",  "seventy",  "eighty",  "ninety",];  
-  let rez = "";
+  let tensNumber = [ "", "twenty",  "thirty",  "forty",  "fifty",  "sixty",  "seventy",  "eighty",  "ninety",];  
+  let qqqq = [];
 
-  if ( number < 10 ) {
-      rez = returnSinglerNumber( number )
+  if ( number < 10 ) {     
+      qqqq.push(returnSinglerNumber( number ));
   };
   if ( number >= 10  && number <20 ) {
-      rez = returnRoundTens( number );
+      qqqq.push(returnRoundTens( number ))
   }
   if ( number >= 20 ) {
     let hundreds = Math.floor( number / 100) ;
-    if ( hundreds > 0 ) {
-        rez += returnSinglerNumber(hundreds)+ " hundred"
+    if ( hundreds > 0 ) {        
+        qqqq.push(returnSinglerNumber(hundreds));
+        qqqq.push("hundred");
     };
     number = number - hundreds*100; 
-    if ( number >= 20 ) {
-        let tens = number % 10;
-        rez += " "+tensNumber[ tens ]
+    let tens = Math.floor(number / 10)
+    if ( number >= 20 ) {               
+        qqqq.push(tensNumber[ tens - 1 ]);
+        number = number - tens*10;
+        if ( number > 0 ) {
+            if ( number < 10 ) {                
+                qqqq.push( returnSinglerNumber( number ) )
+            };
+            if ( number >= 10  && number <20 ) {               
+                qqqq.push( returnRoundTens( number ) )
+            }
+        }
+    }
+    else {       
+        if ( number > 0 ) {
+            if ( number < 10 ) {                
+                qqqq.push( returnSinglerNumber( number ) );
+            };
+            if ( number >= 10  && number <20 ) {                
+                qqqq.push(returnRoundTens( number )  );
+            }
+        }
     }    
+    
   }   
   
-  return rez;
+  return qqqq.join(" ");
 }
 
 
